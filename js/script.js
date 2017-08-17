b6df33b70af7d380cfdc9d0f916326d4beb151cd
flan the dev <flanthedev@gmail.com>
2017-08-17 22:35:30 +0800

    add another image, create slider object

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
-const imgElement = document.getElementById('img');
-const buttonElements = document.getElementsByTagName('button');
-const imgNames = [ 'img/img0.jpg', 'img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg']
 
-imgElement.setAttribute('src', imgNames[0]);
+const buttonElements = document.getElementsByTagName('button');
+
+const slider = {
+  imgNames: [ 'img/img0.jpg', 'img/img1.jpg', 'img/img2.jpg',
+              'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg'],
+
+  imgElement: document.getElementById('img'),
+
+  start: function() {
+    setInterval( function() {
+      slider.showNext();
+    }, 5000);
+  },
+
+  showPrev: function() {
+    let prev = this.imgnames.indexOf( this.imgElement.getAttribute('src') ) - 1;
+    if (prev === -1) {
+      prev = this.imgnames.length - 1;
+    }
+    this.imgElement.setAttribute('src', this.imgnames[prev]);
+  },
+
+  showNext: function() {
+    let next = this.imgnames.indexOf( this.imgElement.getAttribute('src') ) + 1;
+    if (next === this.imgnames.length) {
+      next = 0;
+    }
+    this.imgElement.setAttribute('src', this.imgnames[next]);
+  }
+}
+
+
+slider.imgElement.setAttribute('src', slider.imgNames[0]); // place first image
+
+let interval = setInterval( function() {
+  slider.showNext();
+}, 5000);
 
 buttonElements[0].onclick = () => {
-  let prev = imgNames.indexOf( imgElement.getAttribute('src') ) - 1;
-  if (prev === -1) {
-    prev = imgNames.length - 1;
-  }
-  imgElement.setAttribute('src', imgNames[prev]);
+  slider.showPrev();
+  clearInterval(interval);
+  slider.start();
 }
 
 buttonElements[1].onclick = () => {
-  let next = imgNames.indexOf( imgElement.getAttribute('src') ) + 1;
-  if (next === imgNames.length) {
-    next = 0;
-  }
-  imgElement.setAttribute('src', imgNames[next]);
+  slider.showNext();
+  clearInterval(interval);
+  slider.start();
 }
