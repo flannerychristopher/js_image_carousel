7b14995ce36dc5d4d1192277d936c92771f0a2a5
flan the dev <flanthedev@gmail.com>
2017-08-17 22:45:20 +0800

    hovering image pauses slides

--- js/script.js
+++ js/script.js
@@ -8,43 +8,48 @@ const slider = {
   imgElement: document.getElementById('img'),
 
   start: function() {
-    setInterval( function() {
+    interval = setInterval( function() { // begins movement
       slider.showNext();
     }, 5000);
+
+    this.imgElement.addEventListener('mouseover', () => { // pauses
+      this.pause();
+    });
+    this.imgElement.addEventListener('mouseout', () => {
+      this.start();
+    });
+  },
+
+  pause: function() {
+    clearInterval(interval);
   },
 
   showPrev: function() {
-    let prev = this.imgnames.indexOf( this.imgElement.getAttribute('src') ) - 1;
+    let prev = this.imgNames.indexOf( this.imgElement.getAttribute('src') ) - 1;
     if (prev === -1) {
-      prev = this.imgnames.length - 1;
+      prev = this.imgNames.length - 1;
     }
-    this.imgElement.setAttribute('src', this.imgnames[prev]);
+    this.imgElement.setAttribute('src', this.imgNames[prev]);
   },
 
   showNext: function() {
-    let next = this.imgnames.indexOf( this.imgElement.getAttribute('src') ) + 1;
-    if (next === this.imgnames.length) {
+    let next = this.imgNames.indexOf( this.imgElement.getAttribute('src') ) + 1;
+    if (next === this.imgNames.length) {
       next = 0;
     }
-    this.imgElement.setAttribute('src', this.imgnames[next]);
+    this.imgElement.setAttribute('src', this.imgNames[next]);
   }
 }
 
 
 slider.imgElement.setAttribute('src', slider.imgNames[0]); // place first image
+slider.start(); // start the slider
 
-let interval = setInterval( function() {
-  slider.showNext();
-}, 5000);
 
 buttonElements[0].onclick = () => {
   slider.showPrev();
-  clearInterval(interval);
-  slider.start();
 }
 
 buttonElements[1].onclick = () => {
   slider.showNext();
-  clearInterval(interval);
-  slider.start();
 }
